e2c97ff0f4d4e39db23168143ac771756e1482f6
Jose Delgado <jdelgado1025@gmail.com>
2022-09-06 21:25:09 -0700

    User is not Authorized

--- Controllers/TokenController.cs
+++ Controllers/TokenController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using NuGet.Protocol.Core.Types;
+using static HPlusSport.Web.Areas.Identity.Pages.Account.LoginModel;
 
 namespace HPlusSport.Web.Controllers
 {
@@ -15,6 +16,20 @@ namespace HPlusSport.Web.Controllers
         public TokenController(UserManager<HPlusSportWebUser> userManager)
         {
             _userManager = userManager;
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public async Task<IActionResult> Login([FromBody] InputModel model)
+        {
+            //Check if the user exists
+            var user = await _userManager.FindByEmailAsync(model.Email);
+
+            //User doesn't exist or the password does not match
+            if(user == null || !await _userManager.CheckPasswordAsync(user, model.Password))
+            {
+                return Unauthorized(user);
+            }
         }
     }
 }
